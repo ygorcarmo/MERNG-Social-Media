88273df7a0f4d893ede7689c8e70d4f42d9143e1
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-01-26 17:55:13 +1100

    added user and post model

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const Tech = require('./Tech');
-const Matchup = require('./Matchup');
+const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { Tech, Matchup };
+module.exports = { User, Post };
