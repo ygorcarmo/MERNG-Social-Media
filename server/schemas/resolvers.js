78d5348324ea2c0614032bf0489d1c4aa22e914d
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-01-26 18:13:43 +1100

    added get post route

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,29 +1,42 @@
-const { Tech, Matchup } = require('../models');
+const { User, Post } = require('../models');
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
-    },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
-    },
+    // user: async () => {
+    //   return User.find({});
+    // },
+    // posts: async (parent, { _id }) => {
+    //   const params = _id ? { _id } : {};
+    //   return Post.find(params);
+    // },
+    async getPosts(){
+      try{
+        const posts = await Post.find();
+        return posts;
+      }
+      catch(err){
+        throw new Error(err);
+      }
+    }
   },
-  Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
-    },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
-    },
-  },
+  // Mutation: {
+  //   createUser: async (parent, args) => {
+  //     const user = await User.create(args);
+  //     return user;
+  //   },
+  //   createPost: async (parent, args) => {
+  //     const post = await Post.create(args);
+  //     return post;
+  //   },
+  //   // createVote: async (parent, { _id, techNum }) => {
+  //   //   const vote = await Matchup.findOneAndUpdate(
+  //   //     { _id },
+  //   //     { $inc: { [`tech${techNum}_votes`]: 1 } },
+  //   //     { new: true }
+  //   //   );
+  //   //   return vote;
+  //   // },
+  // },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,40 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Tech {
-    _id: ID!
-    name: String!
+  type Post{
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
   }
-
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
-  }
-
-  type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
-  }
-
-  type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
+  
+ type Query {
+    getPosts: [Post]
   }
 `;
+
+// stuff that I will need later
+// type Tech {
+//   _id: ID!
+//   name: String!
+// }
+
+// type Matchup {
+//   _id: ID!
+//   tech1: String!
+//   tech2: String!
+//   tech1_votes: Int
+//   tech2_votes: Int
+// }
+
+// type Query {
+//   tech: [Tech]
+//   matchups(_id: String): [Matchup]
+// }
+
+// type Mutation {
+//   createMatchup(tech1: String!, tech2: String!): Matchup
+//   createVote(_id: String!, techNum: Int!): Matchup
+// }
 
 module.exports = typeDefs;
