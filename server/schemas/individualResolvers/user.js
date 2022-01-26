1b51d49ed573f9f46439960f7a52a448cd4271c2
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-01-26 18:49:04 +1100

    started with user registration

--- server/schemas/individualResolvers/user.js
+++ server/schemas/individualResolvers/user.js
@@ -1,3 +1,12 @@
 const { User } = require('../../models');
 
-module.exports = {};
+module.exports = {
+    Mutation: {
+        register(_, args, context, info){
+            // TODO: Validate user data
+            // TODO: Make sure user does not already exist
+            // TODO: hash password and create an auth token
+            
+        }
+    }
+};
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,28 +1,13 @@
 const postResolver = require('./individualResolvers/post');
-// const userResolver = require('./individualResolvers/user');
+const userResolver = require('./individualResolvers/user');
 
 const resolvers = {
   Query: {
     ...postResolver.Query
   },
-  // Mutation: {
-  //   createUser: async (parent, args) => {
-  //     const user = await User.create(args);
-  //     return user;
-  //   },
-  //   createPost: async (parent, args) => {
-  //     const post = await Post.create(args);
-  //     return post;
-  //   },
-  //   // createVote: async (parent, { _id, techNum }) => {
-  //   //   const vote = await Matchup.findOneAndUpdate(
-  //   //     { _id },
-  //   //     { $inc: { [`tech${techNum}_votes`]: 1 } },
-  //   //     { new: true }
-  //   //   );
-  //   //   return vote;
-  //   // },
-  // },
+  Mutation: {
+    ...userResolver.Mutation
+  }
 };
 
 module.exports = resolvers;
