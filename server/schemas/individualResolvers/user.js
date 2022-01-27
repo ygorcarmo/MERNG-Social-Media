497bedf9056c63d455159931878d0ceeba79eaef
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-01-27 17:53:27 +1100

    added verification to avoid users duplicate

--- server/schemas/individualResolvers/user.js
+++ server/schemas/individualResolvers/user.js
@@ -1,14 +1,19 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { User } = require('../../models');
+const { UserInputError } = require('apollo-server-express')
 
 module.exports = {
     Mutation: {
-        async register(_, { registerInput: { username, email, password, confirmPassword } }, context, info){
+        async register(_, { registerInput: { username, email, password, confirmPassword } }){
             // TODO: Validate user data
             // TODO: Make sure user does not already exist
-            // TODO: hash password and create an auth token
-            
+            const user = await User.findOne({ username });
+            if(user)
+                throw new UserInputError('Username already exists.', { errors: {
+                    username: 'This username already exists.'
+                }})
+            //hash password and create an auth token            
             password = await bcrypt.hash(password, 12);
 
             const newUser = new User({
