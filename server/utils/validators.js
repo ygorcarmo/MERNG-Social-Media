ce1e97615613b54d8ffccb9fb1f4df1f2e745d8f
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-01-27 18:11:05 +1100

    added username, email and password validator

---
+++ server/utils/validators.js
@@ -0,0 +1,30 @@
+module.exports.validateRegisterInput = (
+    username,
+    email,
+    password,
+    confirmPassword
+) => {
+    const errors = {};
+
+    if(username.trim() === '' || username === null)
+        errors.username = 'Invalid username';
+    
+    if(email.trim() === '' || username === null)
+        errors.username = 'Invalid email address';
+    else{
+        const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+        if(!email.match(regEx))
+            errors.email = 'Invalid email address';
+
+    }
+
+    if(password === '' || password === null)
+        errors.password = 'Invalid password'
+    else if(password !== confirmPassword)
+        errors.confirmPassword = 'Passwords do not match';
+
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    }
+}
