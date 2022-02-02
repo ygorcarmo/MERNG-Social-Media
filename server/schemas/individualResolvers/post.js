e93541a08eb543b13ad9019dbfd2fbe3143c26e7
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-02-02 22:32:14 +1100

    added mutation like/dislike post

--- server/schemas/individualResolvers/post.js
+++ server/schemas/individualResolvers/post.js
@@ -1,6 +1,6 @@
 const { Post } = require("../../models");
 const checkAuth = require("../../utils/check-auth");
-const { AuthenticationError } = require("apollo-server-express");
+const { AuthenticationError, UserInputError } = require("apollo-server-express");
 
 module.exports = {
   Query: {
@@ -60,5 +60,25 @@ module.exports = {
         throw new Error(err);
       }
     },
+    async likePost(_, { postId }, context){
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if(post){
+        if(post.likes.find(like => like.username === username ))
+          post.likes = post.likes.filter(like => like.username !== username);
+        else
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+
+        await post.save();
+        return post;
+      }
+      else
+        throw new UserInputError('Post not found');
+    }
   },
 };
